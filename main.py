b46e06974797cb1043e86a1344cc872c4a080fff
Yashwanth Singh <yashwanthsinghgarandal@gmail.com>
2025-02-21 19:18:40 +0530

    The first commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from Networksecurity.entity.config_entity import TrainingPipelineConfig
 from Networksecurity.components.data_ingestion import DataIngestion
 from Networksecurity.components.data_validation import DataValidation
+from Networksecurity.components.data_transformation import DataTransformation
 from Networksecurity.exception.exception import NetworkSecurityException
 from Networksecurity.logging.logger import logging
-from Networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from Networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from Networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -21,8 +22,15 @@ if __name__ =='__main__':
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
         logging.info("Initiate the data validation")
         data_validation_artifact=data_validation.initiate_data_validation()
-        logging.info("data Validation completed")
+        logging.info("Data Validation completed")
         print(data_validation_artifact)
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Data Transformation started")
+        DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data Transformation completed")
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
